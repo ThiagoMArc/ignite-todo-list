f3e6a118129a08cb74cac4e5005f1c4a5fa38f16
Thiago Arcanjo <tf.arcanjo@gmail.com>
2023-06-28 23:26:06 -0300

    Cria funcionalidade de adição de tasks

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,15 +1,31 @@
 import styles from './CreateTask.module.css'
 import {PlusCircle} from 'phosphor-react'
 import { Task } from './Task'
+import { useState } from 'react'
+import { Todo } from '../models/Todo'
 
 export function CreateTask(){
+
+    const [newTodoText, setTodoText] = useState('')
+    const [todos, setTodos] = useState<Todo[]>([])
+
+    function handleCreateTodo(event: any){
+        event.preventDefault()
+        setTodos([...todos, {id: todos.length +1, name: newTodoText, completed:false}])
+        setTodoText('')
+    }
+
+    function handleNewTodoChange(event: any) {
+        setTodoText(event.target.value)
+    }
+
     return(
         <>
-            <div className={styles.wrapper}>
-                <input type="text" placeholder="Adicione uma nova tarefa" />
+            <form onSubmit={handleCreateTodo} className={styles.wrapper}>
+                <input type="text" placeholder="Adicione uma nova tarefa" onChange={handleNewTodoChange} value={newTodoText} />
                 <button title="Criar">Criar <PlusCircle size={20} /></button>
-            </div>
-            <Task/>
+            </form>
+            <Task todos={todos}/>
         </>
     ) 
 }
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,8 +1,12 @@
 import { Trash } from 'phosphor-react'
 import clipBoard from '../assets/clipboard.svg'
 import styles from './Task.module.css'
+import { Todos } from '../models/Todos'
 
-export function Task(){
+export function Task({todos}: Todos){
+
+    const thereIsTodos = todos.length > 0;
+
     return(
         <>
             <div className={styles.taskInfo}>
@@ -10,29 +14,31 @@ export function Task(){
                 <p className={styles.completedTask}>Concluídas <span className={styles.counter}>0</span></p>
             </div>
 
-            {/* <div className={styles.noTasks}>
-                <img src={clipBoard} alt="" />
-                <p className={styles.noTaskRegistered}>Você ainda não possui tarefas cadastradas</p>
-                <p>Crie tarefas e organize seus itens a seguir</p>
-            </div> */}
-            <div className={styles.tasks}>
+            {thereIsTodos ? (
+                <div className={styles.tasks}>
                 <ul>
-                    <li>
-                        <input type="checkbox" title="Completar tarefa" />
-                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-                        <button className={styles.trash} title="Deletar tarefa">
-                            <Trash size={20} />
-                        </button>
-                    </li>
-                    <li>
-                        <input type="checkbox" title="Completar tarefa" />
-                        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-                        <button className={styles.trash} title="Deletar tarefa">
-                            <Trash size={20} />
-                        </button>
-                    </li>
+                    {
+                        todos.map(td => {
+                            return (
+                                <li key={td.id}>
+                                    <input type="checkbox" title="Completar tarefa" />
+                                    {td.name}
+                                    <button className={styles.trash} title="Deletar tarefa">
+                                        <Trash size={20} />
+                                    </button>
+                                </li>
+                            )        
+                        })
+                    }
                 </ul>
-            </div>            
+            </div>
+            ) : (
+                <div className={styles.noTasks}>
+                    <img src={clipBoard} alt="" />
+                    <p className={styles.noTaskRegistered}>Você ainda não possui tarefas cadastradas</p>
+                    <p>Crie tarefas e organize seus itens a seguir</p>
+                </div>
+            )}      
         </>
     )
 }
